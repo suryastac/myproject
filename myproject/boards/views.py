1a777ade004decc342bd16a80b0730734dbeccf3
suryastac <suryamohan2801@gmail.com>
2023-07-14 14:06:13 +0530

    first commit

---
+++ myproject/boards/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+# def home(request):
+#     return render(request,'index.html')
+# def add(request):
+#     value1=int(request.GET["num1"])
+#     value2=int(request.GET["num2"])
+#     res=value1+value2
+#     return render(request,'result.html',{'result':res})
